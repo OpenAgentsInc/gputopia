5dfd392e7ab259a496c46832eaa4cef00dbb8900
Christopher David <chris@arcadelabs.co>
2023-09-16 18:28:35 -0400

    Save alby access tokens and when theyll expire

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useEffect } from "react"
-import { ModeToggle } from "@/components/ui/mode-toggle"
 
 const tokenEndpoint = "https://api.getalby.com/oauth/token";
 const clientId = process.env.NEXT_PUBLIC_ALBY_CLIENT_ID;
@@ -34,7 +33,23 @@ export default function Auth() {
         })
           .then(response => response.json())
           .then(data => {
-            console.log(data)
+            const {
+              access_token,
+              expires_in,
+              refresh_token,
+              scope,
+              token_type
+            } = data;
+            // Get current time in seconds so we can see exactly when the Alby token will expire
+            const currentTime = Math.floor(Date.now() / 1000);
+            const expires_at = currentTime + expires_in;
+            window.sessionStorage.setItem("alby_access_token", access_token);
+            window.sessionStorage.setItem("alby_expires_at", expires_at.toString());
+            window.sessionStorage.setItem("alby_expires_in", expires_in.toString());
+            window.sessionStorage.setItem("alby_refresh_token", refresh_token);
+            window.sessionStorage.setItem("alby_scope", scope);
+            window.sessionStorage.setItem("alby_token_type", token_type);
+            window.location.href = `/`;
           });
       }
     }
