7e1c9943d75468f8f4e99f80523bf83aa5548c92
Christopher David <chris@arcadelabs.co>
2023-09-16 07:33:29 -0400

    Check for WebLN

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
+"use client"
+
+interface WebLN {
+  // todo
+}
+
+declare global {
+  interface Window {
+    webln: WebLN;
+  }
+}
+
+import { useEffect } from "react"
 
 export default function Home() {
+
+  useEffect(() => {
+    if (typeof window.webln !== 'undefined') {
+      console.log('WebLN is available!');
+    }
+  }, [])
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
