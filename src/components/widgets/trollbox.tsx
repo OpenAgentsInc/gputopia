040eebcb316c81a6ab64d46c60f58000f8e05ee3
Christopher David <chris@arcadelabs.co>
2023-09-20 09:51:37 -0400

    Add trollbox to page & resize

--- src/components/widgets/trollbox.tsx
+++ src/components/widgets/trollbox.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 export const Trollbox = () => {
   return (
-    <Card>
+    <Card className="flex flex-col max-h-[calc(100vh-280px)]">
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle className="mb-2 text-sm font-medium text-muted-foreground">
           Trollbox
