b1aa3c7bad3c85ac3d8e8296dc3b095669a73124
Christopher David <chris@arcadelabs.co>
2023-09-21 16:44:23 -0400

    Improve button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           <MainNav className="mx-6" />
           <div className="ml-auto flex items-center space-x-4">
             <ModeToggle />
-            {user ? <UserNav user={user} logout={logout} /> : <Button onClick={startAlbyOauth}>Log in with Alby</Button>}
+            {user ? <UserNav user={user} logout={logout} /> : <Button variant="outline" onClick={startAlbyOauth}>Log in with Alby</Button>}
           </div>
         </div>
       </div>
@@ -39,12 +39,23 @@ export default function Home() {
           // filter: 'blur(0.8px)'
         }}>
         </div>
-        <div className="text-center">
-          <h1 className="text-4xl font-bold tracking-tight sm:text-6xl text-white" >Your GPU Marketplace</h1>
-          <p className="mt-6 mb-12 text-lg leading-8" style={{ color: "#d3d3d3" }}>The easiest way to buy and sell GPU capacity.</p>
-          <a href="/beta" className="tracking-wider font-medium big-green-button rounded-xl px-5 py-4 text-lg text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">
-            Join the Beta
-          </a>
+        <div className="-mt-16 mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+            <div className="-mt-48 sm:mt-0 mb-8 flex justify-center">
+              <a href="/intro">
+                <div className="tracking-wider shentox relative rounded-full px-3 py-1 text-md leading-6 text-gray-400 hover:text-white ring-1 ring-white/10">
+                  READ: Toward an Open GPU Mesh
+                </div>
+              </a>
+            </div>
+            <div className="text-center">
+              <h1 className="text-4xl font-bold tracking-tight sm:text-6xl text-white" >Your GPU Marketplace</h1>
+              <p className="mt-6 mb-12 text-lg leading-8" style={{ color: "#d3d3d3" }}>The easiest way to buy and sell GPU capacity.</p>
+              <a href="/beta" className="tracking-wider font-medium big-green-button rounded-xl px-5 py-4 text-lg text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">
+                Join the Beta
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
