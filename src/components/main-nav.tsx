02080805cdbf6b4814f6340e0163728b5e39f412
Christopher David <chris@arcadelabs.co>
2023-09-21 16:55:59 -0400

    Simplify logo on topnav

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { cn } from "@/lib/utils"
+import { Button } from "./ui/button"
 
 export function MainNav({
   className,
@@ -15,21 +16,8 @@ export function MainNav({
         className="text-sm font-medium transition-colors hover:text-primary -m-1.5 p-1.5 flex flex-row items-center"
         style={{ height: 60 }}
       >
-        {/* <img className=" h-auto w-12" src="/images/logo.png" alt="" /> */}
-        <h1 className="ml-2 headline font-mono">GPUtopia</h1>
+        <Button variant="ghost">GPUtopia</Button>
       </Link>
-      {/*
-      <a href="/" class="-m-1.5 p-1.5 flex flex-row items-center" style="height: 60px">
-        <img class=" h-auto w-12" src="{{ asset('logo.png') }}" alt="">
-        <h1 class="ml-2 headline">GPUTOPIA</h1>
-      </a> */}
-
-      {/* <Link
-        href="/payments"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        Payments
-      </Link> */}
     </nav>
   )
 }
