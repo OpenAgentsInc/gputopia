25f5563aa2a676020793794824fef671256e5204
Christopher David <chris@arcadelabs.co>
2023-09-22 10:27:05 -0400

    Blog format

--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -1,33 +1,21 @@
 "use client"
 
-import { MainNav } from "@/components/main-nav"
-import { PusherConnector } from "@/components/pusher-connector"
-import { Button } from "@/components/ui/button"
-import { ModeToggle } from "@/components/ui/mode-toggle"
-import { UserNav } from "@/components/user-nav"
-import { startAlbyOauth } from "@/lib/alby-oauth"
-import { useAlby } from "@/lib/useAlby"
+import { TopNav } from "@/components/top-nav"
+import {
+    ExternalLink, TypographyH1 as H1, TypographyH2 as H2, TypographyH3 as H3,
+    TypographyP as P
+} from "@/components/ui/typography"
 
 export default function Intro() {
-  const { authed, logout, user } = useAlby()
+
   return (
     <div className="flex flex-col h-screen">
-      {authed && <PusherConnector />}
-      <div className="border-b">
-        <div className="flex h-16 items-center px-4">
-          <MainNav className="mx-6" />
-          <div className="ml-auto flex items-center space-x-4">
-            <ModeToggle />
-            {user ? <UserNav user={user} logout={logout} /> : <Button variant="outline" onClick={startAlbyOauth}>Log in with Alby</Button>}
-          </div>
-        </div>
-      </div>
+      <TopNav />
 
       <div className="flex flex-col flex-grow justify-center">
         <div style={{
           backgroundImage: "url('/images/flares2.png')",
           opacity: 0.5,
-          // position: 'absolute',
           top: 65,
           right: 0,
           bottom: 0,
@@ -40,98 +28,97 @@ export default function Intro() {
         >
         </div>
 
-        <div className="relative isolate overflow-hidden pb-16 pt-24 sm:pb-20 min-h-screen flex flex-col justify-center">
+        <div className="relative isolate overflow-hidden pb-16 pt-16 sm:pb-20 min-h-screen flex flex-col justify-center">
           <div className="mx-auto max-w-2xl p-4 sm:p-0">
             <time dateTime="2022-09-05" className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500">
               <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
               <span className="ml-3">September 12, 2023</span>
             </time>
 
-            <h1 className="mt-8 mb-10 text-4xl font-bold tracking-tight sm:text-5xl">Toward an Open GPU Mesh</h1>
+            <H1 className="mt-8">Toward an Open GPU Mesh</H1>
 
-            <p><strong className="italic">The
+            <P><strong className="italic">The
               GPU arms race has begun.</strong>
-            </p>
+            </P>
 
-            <p>
-              Companies large and small are <a href="https://gpus.llm-utils.org/nvidia-h100-gpus-supply-and-demand/">scrambling
-              </a>
-              to hoard GPUs, which are now "considerably harder to get than drugs." [<a className="-ml-1" href="https://youtu.be/nxbZVH9kLao?t=30">Elon</a><span className="-ml-1">]</span>
-            </p>
+            <P>
+              Companies large and small are <ExternalLink href="https://gpus.llm-utils.org/nvidia-h100-gpus-supply-and-demand/">scrambling
+              </ExternalLink> to hoard GPUs, which are now "considerably harder to get than drugs." [<ExternalLink href="https://youtu.be/nxbZVH9kLao?t=30">Elon</ExternalLink>]
+            </P>
 
-            <p>Some large companies are not only stockpiling GPUs, they are actively <a href="https://twitter.com/JosephJacks_/status/1698139860383780924">lobbying</a> governments to
+            <P>Some large companies are not only stockpiling GPUs, they are actively <ExternalLink href="https://twitter.com/JosephJacks_/status/1698139860383780924">lobbying</ExternalLink> governments to
               establish "safety" regulations, creating barriers for smaller players.
-            </p>
+            </P>
 
-            <p>
-              It's the familiar pattern of <a href="https://wiki.mises.org/wiki/Regulatory_capture">regulatory capture</a> dressed in sheep's clothing, a blatant
+            <P>
+              It's the familiar pattern of <ExternalLink href="https://wiki.mises.org/wiki/Regulatory_capture">regulatory capture</ExternalLink> dressed in sheep's clothing, a blatant
               power play to stifle competition.
-            </p>
+            </P>
 
-            <p>
+            <P>
               But what if we could level the playing field?
-            </p>
+            </P>
 
-            <p>
+            <P>
               <strong className="italic">What if we could enable unstoppable, universal access to GPU compute?</strong>
-            </p>
+            </P>
 
 
-            <h2 className="font-bold text-2xl mt-10">Introducing GPUtopia</h2>
+            <H2 className="mt-10">Introducing GPUtopia</H2>
 
-            <p>
+            <P>
               You, the person reading this, likely own or can access one or more computers with unused GPU capacity.
-            </p>
+            </P>
 
-            <p>
+            <P>
               <strong className="italic">Would you sell that extra GPU capacity if you could?</strong>
-            </p>
+            </P>
 
-            <p>
+            <P>
               We think a lot of people would.
-            </p>
+            </P>
 
-            <p>
+            <P>
               So what if we connect all such devices together into <strong>one global mesh network for GPU compute?</strong>
-            </p>
+            </P>
 
-            <p>
-              This is not a new idea. There are a handful of existing projects with the same goal, some of which have <a href="https://decrypt.co/144068/gensyn-ai-secures-43m-for-decentralized-machine-learning-led-by-a16z">raised</a> millions of dollars, but they all seem to have some garbage crypto token or convoluted crypto "governance" mechanism. Hilarious and unnecessary.
-            </p>
+            <P>
+              This is not a new idea. There are a handful of existing projects with the same goal, some of which have <ExternalLink href="https://decrypt.co/144068/gensyn-ai-secures-43m-for-decentralized-machine-learning-led-by-a16z">raised</ExternalLink> millions of dollars, but they all seem to have some garbage crypto token or convoluted crypto "governance" mechanism. Hilarious and unnecessary.
+            </P>
 
-            <p>
+            <P>
               Connecting buyers and sellers of GPU compute should be simple.
-            </p>
+            </P>
 
-            <p>
+            <P>
               No gatekeeping or quotas or waitlists or whitepapers or tokens, just a simple marketplace with pricing set through flexible supply and demand.
-            </p>
+            </P>
 
-            <p>
-              Pay with any usual payment method and get the compute you need. And if you want the best rates, avoid any possibility of chargeback fraud by paying and earning in <a href="https://bitcoin.org/">bitcoin</a><span className="-ml-1">.</span>
-            </p>
+            <P>
+              Pay with any usual payment method and get the compute you need. And if you want the best rates, avoid any possibility of chargeback fraud by paying and earning in <ExternalLink href="https://bitcoin.org/">bitcoin</ExternalLink>.
+            </P>
 
-            <p>
+            <P>
               That's what we're building. Now we'd love your help to test what we've built so far.
-            </p>
+            </P>
 
-            <h2 className="font-bold text-2xl mt-10">Join us</h2>
+            <H2 className="mt-10">Join us</H2>
 
-            <p>
+            <P>
               Today we launched the beta version of our GPU seller experience. It enables anyone to sell their GPU compute for bitcoin using nothing but their web browser.
-            </p>
+            </P>
 
-            <p>
+            <P>
               Later this month we'll launch the buyer experience, enabling anyone to easily purchase that compute for a variety of AI/ML workloads.
-            </p>
+            </P>
 
-            <p>
+            <P>
               Over the coming weeks and months, we will rapidly add support for more devices and more workloads. We'll prioritize new features according to the feedback of our active users, so please get involved and tell us what you'd love to see.
-            </p>
+            </P>
 
-            <p>
+            <P>
               <strong className="italic">See you in GPUtopia!</strong>
-            </p>
+            </P>
           </div></div>
       </div>
     </div>
---
+++ src/components/top-nav.tsx
@@ -0,0 +1,25 @@
+import { MainNav } from "@/components/main-nav"
+import { PusherConnector } from "@/components/pusher-connector"
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/ui/mode-toggle"
+import { UserNav } from "@/components/user-nav"
+import { startAlbyOauth } from "@/lib/alby-oauth"
+import { useAlby } from "@/lib/useAlby"
+
+export const TopNav = () => {
+  const { authed, logout, user } = useAlby()
+  return (
+    <>
+      {authed && <PusherConnector />}
+      <div className="border-b">
+        <div className="flex h-16 items-center px-4">
+          <MainNav className="mx-6" />
+          <div className="ml-auto flex items-center space-x-4">
+            <ModeToggle />
+            {user ? <UserNav user={user} logout={logout} /> : <Button variant="outline" onClick={startAlbyOauth}>Log in with Alby</Button>}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
