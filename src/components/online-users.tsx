ed42e57ec4fb4a7978e5446096989fd406e91f8d
Christopher David <chris@arcadelabs.co>
2023-09-22 13:28:06 -0400

    Connected users number

--- src/components/online-users.tsx
+++ src/components/online-users.tsx
@@ -1,3 +1,6 @@
+import {
+    Tooltip, TooltipContent, TooltipProvider, TooltipTrigger
+} from "@/components/ui/tooltip"
 import { useStore } from "@/lib/store"
 import { PersonIcon } from "@radix-ui/react-icons"
 
@@ -5,9 +8,18 @@ export const OnlineUsers = () => {
   const onlineMembers = useStore((state) => state.onlineMembers)
   if (onlineMembers <= 0) return <></>
   return (
-    <span className="flex flex-row items-center text-muted-foreground">
-      <PersonIcon className="h-4 w-4 text-sm" />
-      <p className="text-sm mx-1">{onlineMembers}</p>
-    </span>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <span className="flex flex-row items-center text-muted-foreground">
+            <PersonIcon className="h-4 w-4 text-sm" />
+            <p className="text-sm mx-1">{onlineMembers}</p>
+          </span>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Connected users</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
