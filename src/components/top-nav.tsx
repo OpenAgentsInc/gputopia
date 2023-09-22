088aaf647716d945ad0779cb5978e00dea6b84d4
Christopher David <chris@arcadelabs.co>
2023-09-22 13:57:18 -0400

    Don't show login button while authing

--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { usePathname } from "next/navigation"
 import { MainNav } from "@/components/main-nav"
 import { PusherConnector } from "@/components/pusher-connector"
 import { Button } from "@/components/ui/button"
@@ -7,9 +8,11 @@ import { UserNav } from "@/components/user-nav"
 import { startAlbyOauth } from "@/lib/alby-oauth"
 import { useAlby } from "@/lib/useAlby"
 import { OnlineUsers } from "./online-users"
+import { SatsBalance } from "./sats-balance"
 
 export const TopNav = () => {
   const { authed, logout, user } = useAlby()
+  const pathname = usePathname()
   return (
     <>
       {authed && <PusherConnector />}
@@ -17,8 +20,13 @@ export const TopNav = () => {
         <div className="flex h-16 items-center px-4">
           <MainNav className="mx-6" />
           <div className="ml-auto flex items-center space-x-4">
-            <OnlineUsers />
-            {user ? <UserNav user={user} logout={logout} /> : <Button variant="outline" onClick={startAlbyOauth}>Log in with Alby</Button>}
+            {authed && <OnlineUsers />}
+            {authed && <SatsBalance />}
+            {pathname !== '/auth' && (
+              <>
+                {user ? <UserNav user={user} logout={logout} /> : <Button variant="outline" onClick={startAlbyOauth}>Log in with Alby</Button>}
+              </>
+            )}
           </div>
         </div>
       </div>
