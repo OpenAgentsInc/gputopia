32550deb9f11a58109d8e46cabf1f55649ecaa66
Christopher David <chris@arcadelabs.co>
2023-09-22 14:49:21 -0400

    Bitcoin sats balance

---
+++ src/lib/useBalance.ts
@@ -0,0 +1,27 @@
+import { useEffect } from "react"
+import { useStore } from "./store"
+
+export function useBalance() {
+  const balance = useStore((state) => state.balance)
+  const user = useStore((state) => state.user)
+  useEffect(() => {
+    if (!user) return
+
+    fetch("/api/balance", {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: user.email }),
+    }).then((res) => {
+      if (res.ok) {
+        return res.json()
+      }
+    }).then((json) => {
+      if (json.totalSatsEarned) {
+        useStore.setState({ balance: json.balance, totalSatsEarned: json.totalSatsEarned })
+      }
+    }).catch((error) => {
+      console.log(error);
+    })
+  }, [user?.email])
+  return balance
+}
