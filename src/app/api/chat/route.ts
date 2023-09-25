fb3c4a93fbde24da57c3b0aa1b0c3db7bbef81c3
Christopher David <chris@arcadelabs.co>
2023-09-25 06:51:18 -0400

    Check user balance

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,9 +1,18 @@
+import mysql from "mysql2/promise"
 import { NextRequest, NextResponse } from "next/server"
 import { pusher } from "@/lib/pusher"
 import { kv } from "@vercel/kv"
 
 export async function POST(req: NextRequest) {
   const json = await req.json();
+
+  // Get the user ID
+  const userIdString = req.cookies.get('userId');
+  if (!userIdString) {
+    throw new Error("Missing user ID cookie");
+  }
+  const userId = Number(userIdString.value);
+  console.log("Chat from userId:", userId)
 
   const { messages, id } = json;
 
@@ -13,14 +22,17 @@ export async function POST(req: NextRequest) {
   if (lastUserMessage) {
     // Check user balance (replace this with actual SQL code)
     const userBalance = await checkUserBalance(id); // Placeholder function
+    console.log(userBalance + " balance")
 
     if (userBalance >= 7) {
       // Create a job and add it to the Vercel KV queue
-      const job = JSON.stringify({ userId: id, message: lastUserMessage.content, model: 'Vicuna' });
+      const jobObject = { userId: id, message: lastUserMessage.content, model: 'Vicuna' }
+      const job = JSON.stringify(jobObject);
       await kv.rpush('job_queue', job);
 
       // Trigger a "new job" event via Pusher to alert model providers
-      pusher.trigger('my-channel', 'new-job', {});
+      pusher.trigger('v3job', 'new-job', jobObject);
+      console.log("SENT PUSHER EVENT")
 
       // Deduct balance (replace this with actual SQL code)
       await deductUserBalance(id, 7); // Placeholder function
@@ -32,12 +44,30 @@ export async function POST(req: NextRequest) {
   return NextResponse.json("Job queued");
 }
 
-// Placeholder functions
-async function checkUserBalance(userId: number) {
-  // Check user balance from SQL database
-  return 10; // Example balance
+
+async function checkUserBalance(userId: number): Promise<number> {
+  // Create MySQL connection
+  const connection = await mysql.createConnection(process.env.DATABASE_URL as string);
+
+  try {
+    // Check balance without locking the row
+    const [rows] = await connection.execute(
+      'SELECT balance FROM users WHERE id = ?',
+      [userId]
+    ) as any;
+
+    const currentBalance = rows[0]?.balance || 0;
+    return currentBalance;
+
+  } catch (err: any) {
+    throw new Error('Error fetching balance: ' + err.message);
+  } finally {
+    await connection.end(); // Close the connection
+  }
 }
+
 
 async function deductUserBalance(userId: number, amount: number) {
   // Deduct user balance in SQL database
+  console.log(`Placeholder: Deducted ${amount} from user ${userId}`)
 }
