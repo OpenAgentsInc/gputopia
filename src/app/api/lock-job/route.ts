2e2d42e28c481f2b3d4cac5550be5bff4db0f035
Christopher David <chris@arcadelabs.co>
2023-09-26 10:18:43 -0400

    log win or lose job

--- src/app/api/lock-job/route.ts
+++ src/app/api/lock-job/route.ts
@@ -11,5 +11,25 @@ export async function POST(req: NextRequest) {
   // Try to acquire lock
   const lockSet = await kv.setnx(lockKey, 'locked');
 
+  try {
+    // Get the user ID
+    const userIdString = req.cookies.get('userId');
+    if (!userIdString) {
+      throw new Error("Missing user ID cookie");
+    }
+    const userId = Number(userIdString.value);
+
+    if (lockSet === 1) { // Lock acquired
+      console.log(`"WON JOB: user ${userId} locked job ${jobId}`)
+    } else {
+      console.log(`"LOST JOB: user ${userId} lost out on job ${jobId}`)
+    }
+
+  } catch (e) {
+    console.log("Error locking job")
+  }
+
+
+
   return NextResponse.json({ lockSet });
 }
