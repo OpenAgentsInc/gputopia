5b1049dbda8441d9e8db8ef07498a33108b6e580
Christopher David <chris@arcadelabs.co>
2023-09-27 11:10:57 -0400

    Log the update force

--- src/app/api/lock-job/route.ts
+++ src/app/api/lock-job/route.ts
@@ -5,7 +5,15 @@ export async function POST(req: NextRequest) {
   const json = await req.json();
   const { jobId, updated } = json;
 
+  // Get the user ID
+  const userIdString = req.cookies.get('userId');
+  if (!userIdString) {
+    throw new Error("Missing user ID cookie");
+  }
+  const userId = Number(userIdString.value);
+
   if (!updated) {
+    console.log(`Forcing update for user ${userId} on job ${jobId}`)
     throw new Error("Outdated client code, please refresh")
   }
 
@@ -16,13 +24,6 @@ export async function POST(req: NextRequest) {
   const lockSet = await kv.setnx(lockKey, 'locked');
 
   try {
-    // Get the user ID
-    const userIdString = req.cookies.get('userId');
-    if (!userIdString) {
-      throw new Error("Missing user ID cookie");
-    }
-    const userId = Number(userIdString.value);
-
     if (lockSet === 1) { // Lock acquired
       console.log(`"WON JOB: user ${userId} locked job ${jobId}`)
     } else {
