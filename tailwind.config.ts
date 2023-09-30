0531d77f7fe3c5a7d823674fa0abbf7880367772
Christopher David <chris@arcadelabs.co>
2023-09-30 15:31:36 -0400

    Harmonize styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,45 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{js,mjs,jsx,ts,tsx,mdx}',
-  ],
+import { Config, type } from "tailwindcss"
+import headlessuiPlugin from "@headlessui/tailwindcss"
+import typographyPlugin from "@tailwindcss/typography"
+import typographyStyles from "./typography"
+
+export default {
+  content: ['./src/**/*.{js,mjs,jsx,ts,tsx,mdx}'],
+  darkMode: 'class',
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
+    fontSize: {
+      '2xs': ['0.75rem', { lineHeight: '1.25rem' }],
+      xs: ['0.8125rem', { lineHeight: '1.5rem' }],
+      sm: ['0.875rem', { lineHeight: '1.5rem' }],
+      base: ['1rem', { lineHeight: '1.75rem' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      xl: ['1.25rem', { lineHeight: '1.75rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2rem' }],
+      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+      '5xl': ['3rem', { lineHeight: '1' }],
+      '6xl': ['3.75rem', { lineHeight: '1' }],
+      '7xl': ['4.5rem', { lineHeight: '1' }],
+      '8xl': ['6rem', { lineHeight: '1' }],
+      '9xl': ['8rem', { lineHeight: '1' }],
     },
+    typography: typographyStyles,
     extend: {
+      boxShadow: {
+        glow: '0 0 4px rgb(0 0 0 / 0.1)',
+      },
+      maxWidth: {
+        lg: '33rem',
+        '2xl': '40rem',
+        '3xl': '50rem',
+        '5xl': '66rem',
+      },
+      opacity: {
+        1: '0.01',
+        2.5: '0.025',
+        7.5: '0.075',
+        15: '0.15',
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -50,27 +74,8 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+      }
     },
   },
-  plugins: [require("tailwindcss-animate"), require('@tailwindcss/typography')],
-}
+  plugins: [typographyPlugin, headlessuiPlugin],
+} satisfies Config
--- typography-prev.ts
+++ typography-prev.ts
@@ -53,18 +53,18 @@ export default function typographyStyles({ theme }: PluginUtils) {
           '@screen lg': {
             maxWidth: theme('maxWidth.3xl'),
             marginLeft: `calc(50% - min(50%, ${theme('maxWidth.lg')}))`,
-            marginRight: `calc(50% - min(50%, ${theme('maxWidth.lg')}))`,
-          },
+            marginRight: `calc(50% - min(50%, ${theme('maxWidth.lg')}))`
+          }
         },
 
         // Text
         p: {
           marginTop: theme('spacing.6'),
-          marginBottom: theme('spacing.6'),
+          marginBottom: theme('spacing.6')
         },
         '[class~="lead"]': {
           fontSize: theme('fontSize.base')[0],
-          ...theme('fontSize.base')[1],
+          ...theme('fontSize.base')[1]
         },
 
         // Lists
@@ -72,74 +72,74 @@ export default function typographyStyles({ theme }: PluginUtils) {
           listStyleType: 'decimal',
           marginTop: theme('spacing.5'),
           marginBottom: theme('spacing.5'),
-          paddingLeft: '1.625rem',
+          paddingLeft: '1.625rem'
         },
         'ol[type="A"]': {
-          listStyleType: 'upper-alpha',
+          listStyleType: 'upper-alpha'
         },
         'ol[type="a"]': {
-          listStyleType: 'lower-alpha',
+          listStyleType: 'lower-alpha'
         },
         'ol[type="A" s]': {
-          listStyleType: 'upper-alpha',
+          listStyleType: 'upper-alpha'
         },
         'ol[type="a" s]': {
-          listStyleType: 'lower-alpha',
+          listStyleType: 'lower-alpha'
         },
         'ol[type="I"]': {
-          listStyleType: 'upper-roman',
+          listStyleType: 'upper-roman'
         },
         'ol[type="i"]': {
-          listStyleType: 'lower-roman',
+          listStyleType: 'lower-roman'
         },
         'ol[type="I" s]': {
-          listStyleType: 'upper-roman',
+          listStyleType: 'upper-roman'
         },
         'ol[type="i" s]': {
-          listStyleType: 'lower-roman',
+          listStyleType: 'lower-roman'
         },
         'ol[type="1"]': {
-          listStyleType: 'decimal',
+          listStyleType: 'decimal'
         },
         ul: {
           listStyleType: 'disc',
           marginTop: theme('spacing.5'),
           marginBottom: theme('spacing.5'),
-          paddingLeft: '1.625rem',
+          paddingLeft: '1.625rem'
         },
         li: {
           marginTop: theme('spacing.2'),
-          marginBottom: theme('spacing.2'),
+          marginBottom: theme('spacing.2')
         },
         ':is(ol, ul) > li': {
-          paddingLeft: theme('spacing[1.5]'),
+          paddingLeft: theme('spacing[1.5]')
         },
         'ol > li::marker': {
           fontWeight: '400',
-          color: 'var(--tw-prose-counters)',
+          color: 'var(--tw-prose-counters)'
         },
         'ul > li::marker': {
-          color: 'var(--tw-prose-bullets)',
+          color: 'var(--tw-prose-bullets)'
         },
         '> ul > li p': {
           marginTop: theme('spacing.3'),
-          marginBottom: theme('spacing.3'),
+          marginBottom: theme('spacing.3')
         },
         '> ul > li > *:first-child': {
-          marginTop: theme('spacing.5'),
+          marginTop: theme('spacing.5')
         },
         '> ul > li > *:last-child': {
-          marginBottom: theme('spacing.5'),
+          marginBottom: theme('spacing.5')
         },
         '> ol > li > *:first-child': {
-          marginTop: theme('spacing.5'),
+          marginTop: theme('spacing.5')
         },
         '> ol > li > *:last-child': {
-          marginBottom: theme('spacing.5'),
+          marginBottom: theme('spacing.5')
         },
         'ul ul, ul ol, ol ul, ol ol': {
           marginTop: theme('spacing.3'),
-          marginBottom: theme('spacing.3'),
+          marginBottom: theme('spacing.3')
         },
 
         // Horizontal rules
@@ -153,12 +153,12 @@ export default function typographyStyles({ theme }: PluginUtils) {
           marginRight: `calc(-1 * ${theme('spacing.4')})`,
           '@screen sm': {
             marginLeft: `calc(-1 * ${theme('spacing.6')})`,
-            marginRight: `calc(-1 * ${theme('spacing.6')})`,
+            marginRight: `calc(-1 * ${theme('spacing.6')})`
           },
           '@screen lg': {
             marginLeft: `calc(-1 * ${theme('spacing.8')})`,
-            marginRight: `calc(-1 * ${theme('spacing.8')})`,
-          },
+            marginRight: `calc(-1 * ${theme('spacing.8')})`
+          }
         },
 
         // Quotes
@@ -171,13 +171,13 @@ export default function typographyStyles({ theme }: PluginUtils) {
           quotes: '"\\201C""\\201D""\\2018""\\2019"',
           marginTop: theme('spacing.8'),
           marginBottom: theme('spacing.8'),
-          paddingLeft: theme('spacing.5'),
+          paddingLeft: theme('spacing.5')
         },
         'blockquote p:first-of-type::before': {
-          content: 'open-quote',
+          content: 'open-quote'
         },
         'blockquote p:last-of-type::after': {
-          content: 'close-quote',
+          content: 'close-quote'
         },
 
         // Headings
@@ -186,7 +186,7 @@ export default function typographyStyles({ theme }: PluginUtils) {
           fontWeight: '700',
           fontSize: theme('fontSize.2xl')[0],
           ...theme('fontSize.2xl')[1],
-          marginBottom: theme('spacing.2'),
+          marginBottom: theme('spacing.2')
         },
         h2: {
           color: 'var(--tw-prose-headings)',
@@ -194,7 +194,7 @@ export default function typographyStyles({ theme }: PluginUtils) {
           fontSize: theme('fontSize.lg')[0],
           ...theme('fontSize.lg')[1],
           marginTop: theme('spacing.16'),
-          marginBottom: theme('spacing.2'),
+          marginBottom: theme('spacing.2')
         },
         h3: {
           color: 'var(--tw-prose-headings)',
@@ -202,23 +202,23 @@ export default function typographyStyles({ theme }: PluginUtils) {
           ...theme('fontSize.base')[1],
           fontWeight: '600',
           marginTop: theme('spacing.10'),
-          marginBottom: theme('spacing.2'),
+          marginBottom: theme('spacing.2')
         },
 
         // Media
         'img, video, figure': {
           marginTop: theme('spacing.8'),
-          marginBottom: theme('spacing.8'),
+          marginBottom: theme('spacing.8')
         },
         'figure > *': {
           marginTop: '0',
-          marginBottom: '0',
+          marginBottom: '0'
         },
         figcaption: {
           color: 'var(--tw-prose-captions)',
           fontSize: theme('fontSize.xs')[0],
           ...theme('fontSize.xs')[1],
-          marginTop: theme('spacing.2'),
+          marginTop: theme('spacing.2')
         },
 
         // Tables
@@ -228,11 +228,11 @@ export default function typographyStyles({ theme }: PluginUtils) {
           textAlign: 'left',
           marginTop: theme('spacing.8'),
           marginBottom: theme('spacing.8'),
-          lineHeight: theme('lineHeight.6'),
+          lineHeight: theme('lineHeight.6')
         },
         thead: {
           borderBottomWidth: '1px',
-          borderBottomColor: 'var(--tw-prose-th-borders)',
+          borderBottomColor: 'var(--tw-prose-th-borders)'
         },
         'thead th': {
           color: 'var(--tw-prose-headings)',
@@ -240,42 +240,42 @@ export default function typographyStyles({ theme }: PluginUtils) {
           verticalAlign: 'bottom',
           paddingRight: theme('spacing.2'),
           paddingBottom: theme('spacing.2'),
-          paddingLeft: theme('spacing.2'),
+          paddingLeft: theme('spacing.2')
         },
         'thead th:first-child': {
-          paddingLeft: '0',
+          paddingLeft: '0'
         },
         'thead th:last-child': {
-          paddingRight: '0',
+          paddingRight: '0'
         },
         'tbody tr': {
           borderBottomWidth: '1px',
-          borderBottomColor: 'var(--tw-prose-td-borders)',
+          borderBottomColor: 'var(--tw-prose-td-borders)'
         },
         'tbody tr:last-child': {
-          borderBottomWidth: '0',
+          borderBottomWidth: '0'
         },
         'tbody td': {
-          verticalAlign: 'baseline',
+          verticalAlign: 'baseline'
         },
         tfoot: {
           borderTopWidth: '1px',
-          borderTopColor: 'var(--tw-prose-th-borders)',
+          borderTopColor: 'var(--tw-prose-th-borders)'
         },
         'tfoot td': {
-          verticalAlign: 'top',
+          verticalAlign: 'top'
         },
         ':is(tbody, tfoot) td': {
           paddingTop: theme('spacing.2'),
           paddingRight: theme('spacing.2'),
           paddingBottom: theme('spacing.2'),
-          paddingLeft: theme('spacing.2'),
+          paddingLeft: theme('spacing.2')
         },
         ':is(tbody, tfoot) td:first-child': {
-          paddingLeft: '0',
+          paddingLeft: '0'
         },
         ':is(tbody, tfoot) td:last-child': {
-          paddingRight: '0',
+          paddingRight: '0'
         },
 
         // Inline elements
@@ -288,18 +288,18 @@ export default function typographyStyles({ theme }: PluginUtils) {
           transitionTimingFunction: theme('transitionTimingFunction.DEFAULT'),
           '&:hover': {
             color: 'var(--tw-prose-links-hover)',
-            textDecorationColor: 'var(--tw-prose-links-underline)',
-          },
+            textDecorationColor: 'var(--tw-prose-links-underline)'
+          }
         },
         ':is(h1, h2, h3) a': {
-          fontWeight: 'inherit',
+          fontWeight: 'inherit'
         },
         strong: {
           color: 'var(--tw-prose-bold)',
-          fontWeight: '600',
+          fontWeight: '600'
         },
         ':is(a, blockquote, thead th) strong': {
-          color: 'inherit',
+          color: 'inherit'
         },
         code: {
           color: 'var(--tw-prose-code)',
@@ -310,31 +310,31 @@ export default function typographyStyles({ theme }: PluginUtils) {
           paddingLeft: theme('padding[1.5]'),
           boxShadow: 'inset 0 0 0 1px var(--tw-prose-code-ring)',
           backgroundColor: 'var(--tw-prose-code-bg)',
-          fontSize: theme('fontSize.2xs'),
+          fontSize: theme('fontSize.2xs')
         },
         ':is(a, h1, h2, h3, blockquote, thead th) code': {
-          color: 'inherit',
+          color: 'inherit'
         },
         'h2 code': {
           fontSize: theme('fontSize.base')[0],
-          fontWeight: 'inherit',
+          fontWeight: 'inherit'
         },
         'h3 code': {
           fontSize: theme('fontSize.sm')[0],
-          fontWeight: 'inherit',
+          fontWeight: 'inherit'
         },
 
         // Overrides
         ':is(h1, h2, h3) + *': {
-          marginTop: '0',
+          marginTop: '0'
         },
         '> :first-child': {
-          marginTop: '0 !important',
+          marginTop: '0 !important'
         },
         '> :last-child': {
-          marginBottom: '0 !important',
-        },
-      },
+          marginBottom: '0 !important'
+        }
+      }
     },
     invert: {
       css: {
@@ -354,8 +354,8 @@ export default function typographyStyles({ theme }: PluginUtils) {
         '--tw-prose-code-bg': 'var(--tw-prose-invert-code-bg)',
         '--tw-prose-code-ring': 'var(--tw-prose-invert-code-ring)',
         '--tw-prose-th-borders': 'var(--tw-prose-invert-th-borders)',
-        '--tw-prose-td-borders': 'var(--tw-prose-invert-td-borders)',
-      },
-    },
+        '--tw-prose-td-borders': 'var(--tw-prose-invert-td-borders)'
+      }
+    }
   }
 }
