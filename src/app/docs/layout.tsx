44d1efff304a3b410ccd338114bd506e094fd59e
Christopher David <chris@arcadelabs.co>
2023-10-01 14:16:25 -0400

    First pass on docs/FAQ (#7)
    * Remove dialogtrigger from webgpuchecker
    
    * Add blank docs page
    
    * Load MDX config
    
    * Pull in protocol components for docs
    
    * Styling looking good
    
    * Harmonize styles
    
    * Fix buttons not-prose
    
    * axe footer/feedback
    
    * copy
    
    * FAQ page
    
    * Initial FAQ
    
    * page title
    
    * Buy/sell
    
    * Copy and harmonize link color
    
    * ok

---
+++ src/app/docs/layout.tsx
@@ -0,0 +1,52 @@
+// import "@/styles/tailwind.css"
+import glob from "fast-glob"
+import { type Metadata } from "next"
+import { Layout } from "@/components/docs/Layout"
+import { type Section } from "@/components/docs/SectionProvider"
+import { Providers } from "./providers"
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - GPUtopia',
+    default: 'GPUtopia',
+  },
+}
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  let pages = await glob('**/*.mdx', { cwd: 'src/app/docs' })
+  let allSectionsEntries = (await Promise.all(
+    pages.map(async (filename) => [
+      '/' + filename.replace(/(^|\/)page\.mdx$/, ''),
+      (await import(`./${filename}`)).sections,
+    ]),
+  )) as Array<[string, Array<Section>]>
+  let allSections = Object.fromEntries(allSectionsEntries)
+
+  return (
+    <div className="h-full pb-24 bg-zinc-900">
+      <div className="flex min-h-full antialiased">
+        <Providers>
+          <div className="w-full">
+            <Layout allSections={allSections}>{children}</Layout>
+          </div>
+        </Providers>
+      </div>
+    </div>
+  )
+
+  // return (
+  //   <html lang="en" className="h-full" suppressHydrationWarning>
+  //     <body className="flex min-h-full bg-white antialiased dark:bg-zinc-900">
+  //       <Providers>
+  //         <div className="w-full">
+  //           <Layout allSections={allSections}>{children}</Layout>
+  //         </div>
+  //       </Providers>
+  //     </body>
+  //   </html>
+  // )
+}
---
+++ src/app/docs/oldpage.tsx
@@ -0,0 +1,11 @@
+import { BackgroundImage } from "@/components/background-image"
+
+export default function Docs() {
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex flex-col flex-grow justify-center">
+        <BackgroundImage />
+      </div>
+    </div>
+  )
+}
