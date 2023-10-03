68e520a4b3c34ff6d6e0412d78ebc03200fb305f
Christopher David <chris@arcadelabs.co>
2023-10-03 11:08:10 -0400

    blog

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -22,10 +22,10 @@ export function MainNav({
         GPUTOPIA
       </Link>
       <Link
-        href="/blog"
+        href="/blog/opening"
         className={cn(linkClasses, {
-          "text-white": isActive("/intro"),
-          "text-muted-foreground": !isActive("/intro")
+          "text-white": isActive("/blog/opening"),
+          "text-muted-foreground": !isActive("/blog/opening")
         })}
       >
         <span className="mx-2">Blog</span>
