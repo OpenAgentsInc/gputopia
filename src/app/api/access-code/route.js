0b4ce83fa2289c65e05b9509fbb9e46335a0505e
erik aronesty <erik@q32.com>
2023-10-03 14:58:43 -0400

    route

---
+++ src/app/api/access-code/route.js
@@ -0,0 +1,52 @@
+import { NextResponse } from 'next/server'
+import mysql from 'mysql2';
+
+const crypto = require('crypto');
+
+function generateRandomHex(n) {
+    const randomValues = crypto.randomBytes(n);
+    return randomValues.toString('hex');
+}
+
+export async function GET(request) {
+  const userId = Number(request.cookies.get('userId').value);
+  const connection = mysql.createConnection(process.env.DATABASE_URL)
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT access_token FROM users WHERE id = ?', [userId], function (err, results) {
+      connection.end();
+      if (err) {
+        resolve(NextResponse.json({ error: "Database error" }));
+        return;
+      }
+      resolve(NextResponse.json({
+        token: results[0].access_token
+      }));
+    });
+  });
+}
+
+export async function POST(request) {
+  const userId = Number(request.cookies.get('userId').value);
+  const json = await request.json();
+  const connection = mysql.createConnection(process.env.DATABASE_URL)
+  const accessToken = generateRandomHex(16)
+  console.log("user id", userId, "command", json.command)
+  if (json.command == "generate") {
+    return new Promise((resolve, reject) => {
+      connection.query('UPDATE users SET access_token = ? WHERE id = ?', [accessToken, userId], function (err, results) {
+        connection.end();
+
+        if (err) {
+          resolve(NextResponse.json({ error: "Database error" }));
+          return;
+        }
+
+        console.log(results)
+
+        resolve(NextResponse.json({
+          token: accessToken
+        }));
+      });
+    });
+  }
+}
