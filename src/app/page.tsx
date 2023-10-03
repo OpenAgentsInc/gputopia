c4f127008f804c8d2020116f0164351727d9218c
Christopher David <chris@arcadelabs.co>
2023-10-03 12:38:09 -0400

    coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
             </div>
             <div className="text-center">
               <h1 className="text-4xl font-bold tracking-tight sm:text-6xl text-white" >Your GPU Marketplace</h1>
-              <p className="mt-6 mb-12 text-lg leading-8" style={{ color: "#d3d3d3" }}>The easiest way to buy and sell GPU capacity.</p>
+              <p className="mt-6 mb-12 text-lg leading-8" style={{ color: "#d3d3d3" }}>The easiest way to buy and sell GPU capacity. Coming soon.</p>
               <a href="/beta" className="tracking-wider font-medium big-green-button rounded-xl px-5 py-4 text-lg text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">
                 Join the Beta
               </a>
