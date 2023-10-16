01b297352ce9f2871b23446980616ef6cf9b2c06
Christopher David <chris@arcadelabs.co>
2023-10-16 13:19:07 -0400

    Re-add buy/sell web UI (#33)
    * Add beta update
    
    * re-add sellmenu
    
    * Clean out unneeded code in chat func
    
    * Require auth for chat page and put on edge
    
    * Retry without edge
    
    * space

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -15,9 +15,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 export async function POST(req: NextRequest) {
-  console.log('Chat test.')
   const session = await auth()
-  console.log('Here with session:', session)
   if (!session) {
     return new NextResponse('Unauthorized', {
       status: 401
@@ -25,17 +23,7 @@ export async function POST(req: NextRequest) {
   }
 
   const json = await req.json()
-  console.log('json:', json)
   const { messages } = json
-
-  const userId = session.user.id
-  console.log('Checking userId:', userId)
-
-  const userBalance = await checkUserBalance(Number(userId))
-
-  if (userBalance < 7) {
-    return NextResponse.json({ error: 'Insufficient balance' })
-  }
 
   const res = await openai.createChatCompletion({
     model: 'vicuna-7B-q4',
@@ -52,69 +40,3 @@ export async function POST(req: NextRequest) {
 
   return new StreamingTextResponse(stream)
 }
-
-async function checkUserBalance(userId: number): Promise<number> {
-  try {
-    // Horrible balance placeholder so we don't charge users for now
-    const currentBalance = 10
-    return currentBalance
-  } catch (err: any) {
-    throw new Error('Error fetching balance: ' + err.message)
-  }
-}
-
-// async function checkUserBalance(userId: number): Promise<number> {
-//   // Create MySQL connection
-//   const connection = await mysql.createConnection(process.env.DATABASE_URL as string);
-
-//   try {
-//     // Check balance without locking the row
-//     const [rows] = await connection.execute(
-//       'SELECT balance FROM users WHERE id = ?',
-//       [userId]
-//     ) as any;
-
-//     const currentBalance = rows[0]?.balance || 0;
-//     return currentBalance;
-
-//   } catch (err: any) {
-//     throw new Error('Error fetching balance: ' + err.message);
-//   } finally {
-//     await connection.end(); // Close the connection
-//   }
-// }
-
-// export async function deductUserBalance(userId: number, amount: number): Promise<void> {
-//   // Create MySQL connection
-//   const connection = await mysql.createConnection(process.env.DATABASE_URL as string);
-
-//   await connection.beginTransaction();
-//   try {
-//     // Lock the row for the current transaction and check balance
-//     const [rows] = await connection.execute(
-//       'SELECT balance FROM users WHERE id = ? FOR UPDATE',
-//       [userId]
-//     ) as any;
-
-//     const currentBalance = rows[0]?.balance || 0;
-
-//     if (currentBalance < amount) {
-//       throw new Error('Insufficient balance');
-//     }
-
-//     // Deduct balance
-//     await connection.execute(
-//       'UPDATE users SET balance = balance - ? WHERE id = ?',
-//       [amount, userId]
-//     );
-
-//     // Commit the transaction
-//     await connection.commit();
-//   } catch (err: any) {
-//     // Rollback in case of an error
-//     await connection.rollback();
-//     throw new Error('Error deducting balance: ' + err.message);
-//   } finally {
-//     await connection.end(); // Close the connection
-//   }
-// }
