16542d7907ebaf324432814f65441f5850c63204
Christopher David <chris@arcadelabs.co>
2023-10-16 13:11:53 -0400

    Require auth for chat page and put on edge

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,19 @@
+import { auth } from '@/auth'
 import { Chat } from '@/components/chat'
 import { nanoid } from '@/lib/utils'
+import { redirect } from 'next/navigation'
 
-// export const runtime = 'edge'
+export const runtime = 'edge'
+export const preferredRegion = 'home'
 
-export default function IndexPage() {
+export default async function IndexPage() {
   const id = nanoid()
+
+  const session = await auth()
+
+  if (!session?.user) {
+    redirect(`/login`)
+  }
 
   return <Chat id={id} />
 }
