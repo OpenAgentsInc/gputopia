f6a4226800563fba2d7b9ee30b75c3b7564b2a1c
Christopher David <chris@arcadelabs.co>
2023-10-23 16:51:42 -0400

    detach payment api endpoint works

--- src/app/api/payment-methods/route.ts
+++ src/app/api/payment-methods/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion: '2023-10-16' })
@@ -8,7 +8,11 @@ export async function GET() {
 
   const paymentMethods = await stripe.customers.listPaymentMethods(customerId, { type: 'card' })
 
-  console.log('paymentMethods:', paymentMethods)
-
   return NextResponse.json({ paymentMethods: paymentMethods.data })
+}
+
+export async function DELETE(request: NextRequest) {
+  const { id } = await request.json()
+  await stripe.paymentMethods.detach(id)
+  return NextResponse.json({ success: true, detached: id })
 }
--- src/app/billing/components/PaymentMethods.tsx
+++ src/app/billing/components/PaymentMethods.tsx
@@ -5,7 +5,16 @@ import { AddCardDialog } from './AddCardDialog'
 
 export function PaymentMethods() {
   const paymentMethods = usePaymentMethods()
-  // console.log(paymentMethods)
+  const deletePaymentMethod = async (id: string) => {
+    console.log("Let's delete payment method id: ", id)
+    const res = await fetch('/api/payment-methods', {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id })
+    })
+    const data = await res.json()
+    console.log(data)
+  }
   return (
     <Card className="mt-6">
       <CardHeader>
@@ -16,11 +25,9 @@ export function PaymentMethods() {
         {paymentMethods.map(paymentMethod => {
           if (paymentMethod.type !== 'card' || !paymentMethod.card) return null
           const { brand, last4, exp_month, exp_year } = paymentMethod.card
+          const id = paymentMethod.id
           return (
-            <div
-              key={paymentMethod.id}
-              className="mb-2 flex flex-col justify-between border-gray-600 rounded-lg p-3"
-            >
+            <div key={id} className="mb-2 flex flex-col justify-between border-gray-600 rounded-lg p-3">
               <div className="flex items-center">
                 <img src={`/images/${brand}.svg`} alt={brand} className="w-12 mr-3" />
                 <div>
@@ -28,12 +35,19 @@ export function PaymentMethods() {
                   <div className="text-gray-500">
                     Expires {exp_month}/{exp_year}
                   </div>
+                  <Button
+                    onClick={() => deletePaymentMethod(id)}
+                    variant="outline"
+                    size="sm"
+                    className="mt-2 text-sm"
+                  >
+                    Delete
+                  </Button>
                 </div>
               </div>
             </div>
           )
         })}
-
         <AddCardDialog />
       </CardContent>
     </Card>
