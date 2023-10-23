98d816f3ee6882f04ef0f266907eac1244099bda
Christopher David <chris@arcadelabs.co>
2023-10-23 19:50:59 -0400

    use real stripe customer id

--- src/app/api/create-setup-intent/route.ts
+++ src/app/api/create-setup-intent/route.ts
@@ -1,19 +1,28 @@
 import { auth } from '@/auth'
+import { grabStripeCustomerId } from '@/lib/grab-stripe-customer-id'
 import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion: '2023-10-16' })
 
 export async function POST() {
-  // const session = await auth()
-  // if (!session) {
-  //   return new NextResponse('Unauthorized', {
-  //     status: 401
-  //   })
-  // }
+  const session = await auth()
+  if (!session) {
+    return new NextResponse('Unauthorized', {
+      status: 401
+    })
+  }
+  const userId = session.user.user_id
+  // if userId is not a number, return error
+  if (typeof userId !== 'number') {
+    return new NextResponse('Unauthorized', {
+      status: 401
+    })
+  }
 
+  const customerId = await grabStripeCustomerId(userId)
   const setupIntent = await stripe.setupIntents.create({
-    customer: 'cus_OsDSwjUXTQPa2l'
+    customer: customerId
   })
 
   return NextResponse.json({ clientSecret: setupIntent.client_secret })
