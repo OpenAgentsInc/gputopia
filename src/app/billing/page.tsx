ee471460ed16de36e8d3abb6da73edcd2764fe43
Christopher David <chris@arcadelabs.co>
2023-10-22 21:19:55 -0400

    Start billing page

---
+++ src/app/billing/page.tsx
@@ -0,0 +1,7 @@
+export default function Billing() {
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex flex-col flex-grow justify-center"></div>
+    </div>
+  )
+}
