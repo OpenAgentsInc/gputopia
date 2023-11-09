04f72bf04dff2eb13b7ac9dab2c238538d9e1df9
Christopher David <chris@arcadelabs.co>
2023-11-09 08:58:14 -0500

    withdrawal note

--- src/app/balance/page.tsx
+++ src/app/balance/page.tsx
@@ -29,6 +29,7 @@ import { useStore } from '@/lib/store'
 import { useBalance } from '@/lib/useBalance'
 import { withdraw, withdrawInvoice } from '@/lib/withdraw'
 import { useSession } from 'next-auth/react'
+import { RocketIcon } from '@radix-ui/react-icons'
 
 export default function Balance() {
   const { data: session, status } = useSession()
@@ -71,6 +72,18 @@ export default function Balance() {
         <BackgroundImage />
 
         <div className="relative isolate overflow-hidden pb-16 pt-24 sm:pb-20 min-h-screen flex flex-col">
+          <div className="mx-auto max-w-2xl mb-4">
+            <Alert>
+              <RocketIcon className="h-8 w-8" />
+              <AlertTitle className="ml-4 text-lg">Withdrawals fixed</AlertTitle>
+              <AlertDescription className="ml-4 text-muted-foreground">
+                Withdrawals were delayed for a few days, but should now work instantly. All late withdrawals
+                should now have been paid with a 2x bonus. If you are missing any sats, email us
+                info@gputopia.ai with your Alby username and details of what happened.
+              </AlertDescription>
+            </Alert>
+          </div>
+
           <div className="mx-auto max-w-2xl p-4 sm:p-0">
             <Card className="bg-background w-96 mb-8">
               <CardHeader className="flex flex-row items-center justify-between space-y-0 px-6 pb-4">
@@ -156,19 +169,11 @@ export default function Balance() {
 
             <PaymentHistory />
 
-            {/* <Alert>
-              <RocketIcon className="h-8 w-8" />
-              <AlertTitle className="ml-4 text-lg">22 Sept Update</AlertTitle>
-              <AlertDescription className="ml-4 text-muted-foreground">
-                We are close to rolling out version 3 of our beta with the first buyer+seller experience. This page will be updated with details soon. You can prepare by creating your Alby account using the &apos;Log in with Alby&apos; button above.
-              </AlertDescription>
-            </Alert> */}
-
             {/* <H2 className="pt-4 my-8">Bitcoin balance</H2> */}
 
             {/* <P className="mt-12">
               <strong className="text-white text-lg text-center italic">Sats balance: {balance}</strong>
-            </P> */}
+          </P>*/}
           </div>
         </div>
       </div>
